e48902ada28aa52dcb97bd333ddee79822745163
Anas <khananas49340@gmail.com>
2026-08-07 19:57:25 +0530

    completed authentication

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import "dotenv/config";
+import { auth } from "./lib/auth.js";
+import { toNodeHandler } from "better-auth/node";
  
 const app = express();
 const PORT = process.env.PORT || 8081;
- 
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+// Mount express json middleware after Better Auth handler
+// or only apply it to routes that don't interact with Better Auth
  
 app.use(express.json());
 

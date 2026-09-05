5684f9cbf2062cf009cb2a20a70d3970a004fcd6
Anas <khananas49340@gmail.com>
2026-09-06 01:56:19 +0530

    stuck at db

--- server/src/lib/db.ts
+++ server/src/lib/db.ts
@@ -1,24 +1,20 @@
 import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
 };
 
 function createPrismaClient(): PrismaClient {
-    const pool = new pg.Pool({
+    const adapter = new PrismaPg({
         connectionString: process.env.DATABASE_URL,
     });
-    pool.query("SELECT 1")
-  .then(() => console.log("🔥 DATABASE RUNTIME CONNECTED"))
-  .catch((err) => console.error("🔥 DATABASE RUNTIME ERROR:", err));
-    const adapter = new PrismaPg(pool as any);
 
     return new PrismaClient({ adapter });
 }
 
 const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
 prisma.user.findFirst()
   .then((user) => console.log("🔥 PRISMA USER QUERY:", user))
   .catch((err) => console.error("🔥 PRISMA USER QUERY ERROR:", err));
